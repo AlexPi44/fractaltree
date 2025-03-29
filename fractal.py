d0f98454c541f3ccbd9a396fec4912b32b079766
agalexiul <paicualexandru44@gmail.com>
2025-03-29 09:42:26 +0200

    fractal.py

--- fractal.py
+++ fractal.py
@@ -1,16 +1,23 @@
 import turtle
 
+# Constants
+ANGLE = 20
+LENGTH_DECREMENT = 15
+
 def draw_branch(branch_length):
     if branch_length > 5:
         turtle.forward(branch_length)
-        turtle.right(20)
-        draw_branch(branch_length - 15)
-        turtle.left(40)
-        draw_branch(branch_length - 15)
-        turtle.right(20)
+        turtle.right(ANGLE)
+        draw_branch(branch_length - LENGTH_DECREMENT)
+        turtle.left(2 * ANGLE)
+        draw_branch(branch_length - LENGTH_DECREMENT)
+        turtle.right(ANGLE)
         turtle.backward(branch_length)
 
+# Set up the turtle
 turtle.speed(0)
 turtle.left(90)
+
+# Draw the fractal tree
 draw_branch(100)
 turtle.done()
